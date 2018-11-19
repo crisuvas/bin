076354a4612acfd51301364c1976007be7bdf6cf
Cris Ruiz <crisuvas@gmail.com>
2018-11-19 11:07:32 -0600

    Add files via upload

---
+++ bin_and_or_not_xor.py
@@ -0,0 +1,9 @@
+a = 0b00101010
+b = 0b00001111
+
+print(bin(a & b))
+print(bin(a | b))
+# xor
+print(bin(a ^ b))
+# not
+print(bin(~a))
---
+++ bin_function.py
@@ -0,0 +1,4 @@
+shift_right = 0b1100
+
+print(bin(4))
+print(bin(shift_right))
